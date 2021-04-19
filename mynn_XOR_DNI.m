d1130abdbdde221d5d329e78dc1f5afe83e21955
Pablo Trelis <trelispablo@gmail.com>
2021-04-19 19:44:23 +0200

    mynn_XOR fallos

--- mynn_XOR_DNI.m
+++ mynn_XOR_DNI.m
@@ -1,4 +1,4 @@
-function results=mynn_XOR_DNI
+function results=mynn_XOR_DNI(num_ent,num_test)
 %function mynn_XOR_profesor_AND_profesor
 % Funcion principal que realiza las funciones de
  %1) Creación de las variables para el banco de entrenamiento y banco de
@@ -12,15 +12,21 @@ function results=mynn_XOR_DNI
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Creamos entradas y salidas de entrenamiento para una funcion AND
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if nargin<1
+    num_ent=5000;
+end
+if nargin<2
+    num_test=20;
+end
 % Inicializamos las variables E1, E2 y SE ideales para entrenamiento
-VT=5000;                %numero de muestras de entrada
+VT=num_ent;                %numero de muestras de entrada
 NV=round(VT);           %aseguramos que VT sea un numero entero
 E1=round(rand(NV,1));   %vector de NV valores de entrada en pin 1
 E2=round(rand(NV,1));   %vector de NV valores de entrada en pin 2
 SE=double(xor(E1,E2));   %vector salida ideal para las entradas E1 y E
 
 %%%% Creamos entradas y salidas de validacion E1V, E2V, SEV para test
-NVV=20;
+NVV=num_test;
 E1V=round(rand(NVV,1));
 E2V=round(rand(NVV,1));
 SEV=double(xor(E1V,E2V));
@@ -38,18 +44,6 @@ results.error=mean(abs(SEV-S_est));
 
 results.S_est=S_est;
 results.SEV=SEV;
-close all
-figure,
-plot(SEV,'ok','LineWidth',2),hold on
-plot(S_est,'xr','LineWidth',2)
-%set(gcf,'FontWeight','bold')
-set(gca,'FontSize',12) %# Fix font size of the text in the current axes 
-set(gca,'FontWeight','bold')  %# Fix Bold text in the current axes 
-xlabel('Number of test','FontWeight','bold')
-ylabel('Output values','FontWeight','bold')
-axis([-1 length(SEV)+1 -0.1 1.3])
-legend('Correct Values','Perceptron Output')
-title('Evaluation of MULTILAYER Ouput for an XOR','FontWeight','bold')
  
     function [mynn]=initialize_nn(n_inputs)
     %function [myperceptron]=initialize_perceptron(n_inputs)
@@ -65,18 +59,12 @@ title('Evaluation of MULTILAYER Ouput for an XOR','FontWeight','bold')
     rand('state',sum(100*clock));  %inicializa random en función del reloj
     n_neuronas=3;
     for i=1:1:n_neuronas
-        [myperceptron] = inicialize_perceptron(n_inputs);
-        mynn.weights(i,:)=myperceptron.weights;
-        mynn.bias(i)=myperceptron.bias;
-    end
-    function [myperceptron] = inicialize_perceptron(n_inputs)
-        myperceptron.weights=rand(1,n_inputs+1)*2-1;
-        myperceptron.bias=1;
-    end
-    
+        mynn.weights(i,:)=rand(1,n_inputs+1)*2-1;
+        mynn.bias(i)=1;
+    end  
     end
 
-    function mynnT=train_nn(mynn,LR,input,output)
+    function [mynnT]=train_nn(mynn,LR,input,output)
     % funcion que modifica los pesos la red para que vaya aprendiendo 
     % ESTE PERCEPTRON UTILIZA:
         % Funcion sigma SIGMOIDAL
@@ -91,70 +79,76 @@ title('Evaluation of MULTILAYER Ouput for an XOR','FontWeight','bold')
     % OUPUT:
           % mynnT:  estructura con el perceptron ya entrenado
               %mynnT.bias:       %Bias del perceptron (e.g. 1)
-              %mynnT.weights:    %Pesos del perceptron ya entrenado  
-    [N,M]=size(input);
-    for i=1:1:N
-        myperceptron1.weights=mynn.weights(1,:);
-        myperceptron1.bias=mynn.bias(1);
-        myperceptron2.weights=mynn.weights(2,:);
-        myperceptron2.bias=mynn.bias(2);
-        myperceptron3.weights=mynn.weights(3,:);
-        myperceptron3.bias=mynn.bias(3);
-        Y(1)=useperceptron(myperceptron1,input(i));
-        Y(2)=useperceptron(myperceptron2,input(i));
-        Y3=useperceptron(myperceptron2,[Y(1) Y(2)]);
- 
-        e3=Y3*(1-Y3)*(output(i)-Y3);
-        mynn.weights(3,1)=mynn.weights(3,1)+LR*e3*mynn.bias(3);
-        for j=1:1:M
-            mynn.weights(3,j+1)=mynn.weights(3,j+1)+LR*e3*Y(j);
+              %mynnT.weights:    %Pesos del perceptron ya entrenado
+        [N,~]=size(input);
+            myp1.bias=mynn.bias(1);
+            myp2.bias=mynn.bias(2);
+            myp3.bias=mynn.bias(3);
+        for i=1:1:N
+            myp1.weights=mynn.weights(1,:);
+            myp2.weights=mynn.weights(2,:);
+            myp3.weights=mynn.weights(3,:);
+
+            in=input(i,:);
+            out=output(i);
+
+            Y(1)=usep(myp1,in);
+            Y(2)=usep(myp2,in);
+            Y(3)=usep(myp3,[Y(1) Y(2)]);
+
+            e3=Y(3)*(1-Y(3))*(out-Y(3));
+            e1=Y(1)*(1-Y(1))*mynn.weights(3,2)*e3;
+            e2=Y(2)*(1-Y(2))*mynn.weights(3,3)*e3;
+
+            mynn.weights(3,1)=mynn.weights(3,1)+LR*e3*mynn.bias(3);
+            mynn.weights(3,2)=mynn.weights(3,2)+LR*e3*Y(1);
+            mynn.weights(3,3)=mynn.weights(3,3)+LR*e3*Y(2);
+
+            mynn.weights(1,1)=mynn.weights(1,1)+LR*e1*mynn.bias(1);
+            mynn.weights(1,2)=mynn.weights(1,2)+LR*e1*in(1);
+            mynn.weights(1,3)=mynn.weights(1,3)+LR*e1*in(2);
+
+            mynn.weights(2,1)=mynn.weights(2,1)+LR*e2*mynn.bias(2);
+            mynn.weights(2,2)=mynn.weights(2,2)+LR*e2*in(1);
+            mynn.weights(2,3)=mynn.weights(2,3)+LR*e2*in(2);
+
         end
-        e1=Y(1)*(1-Y(1))*mynn.weights(3,2)*e3;
-        e2=Y(2)*(1-Y(2))*mynn.weights(3,3)*e3;
-        mynn.weights(1,1)=mynn.weights(1,1)+LR*e1*mynn.bias(1);
-        for j=1:1:M
-            mynn.weights(1,j+1)=mynn.weights(1,j+1)+LR*e1*input(i,j);
-        end
-        mynn.weights(2,1)=mynn.weights(2,1)+LR*e2*mynn.bias(2);
-        for j=1:1:M
-            mynn.weights(2,j+1)=mynn.weights(2,j+1)+LR*e2*input(i,j);
-        end  
-    end
-    mynnT=mynn;
+        mynnT=mynn;
+
     end
 
-    function out=usenn(mynn,input)
-    % function out=useperceptron(myperceptron,input)
-    % funcion que utiliza el perceptron para calcular las salidas a partir de
-    % las entradas de acuerdo con lo que haya aprendido el perceptron en la
-    % fase de entrenamiento
-    [N,M]=size(input);
-    for i=1:1:N
-        myperceptron1.weights=mynn.weights(1,:);
-        myperceptron1.bias=mynn.bias(1);
-        myperceptron2.weights=mynn.weights(2,:);
-        myperceptron2.bias=mynn.bias(2);
-        myperceptron3.weights=mynn.weights(3,:);
-        myperceptron3.bias=mynn.bias(3);
-        Y(1)=useperceptron(myperceptron1,input(i));
-        Y(2)=useperceptron(myperceptron2,input(i));
-        Y3=useperceptron(myperceptron2,[Y(1) Y(2)]);
-        out(i)=Y3;
-    end
+    function [out]=usenn(mynn,input)
+    [N,~]=size(input);
+        for i=1:1:N
+            in=input(i,:);
+            myp1.weights=mynn.weights(1,:);
+            myp1.bias=mynn.bias(1);
+            myp2.weights=mynn.weights(2,:);
+            myp2.bias=mynn.bias(2);
+            myp3.weights=mynn.weights(3,:);
+            myp3.bias=mynn.bias(3);
+
+            Y(1)=usep(myp1,in);
+            Y(2)=usep(myp2,in);
+            Y3=usep(myp3,[Y(1) Y(2)]);
+            out(i)=Y3;
+        end
+
+
     end
 
-    function [out]=useperceptron(myperceptron,input)
-    [N,M]=size(input);
-    for i=1:1:N
-        for j=1:1:M
-            E(j)=input(i,j);        
-        end
-            v=myperceptron.bias*myperceptron.weights(1);
-        for j=1:1:M
-            v=v+myperceptron.weights(j+1)*E(j);
-        end
-        out(i)=1/(1+exp(-v));   
-    end
+    function [out]=usep(myperceptron,input)
+        M=length(input);
+            for j=1:1:M
+                E(j)=input(j);        
+            end
+                v=myperceptron.bias*myperceptron.weights(1);
+            for j=1:1:M
+                v=v+myperceptron.weights(j+1)*E(j);
+            end
+            %SIGMOIDAL
+            out=1/(1+exp(-v));   
+
     end
 
 end
